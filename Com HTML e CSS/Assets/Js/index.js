c20cdcb7f8efde468f937559e64133fd1fa2f643
PollyStos <polly_stos@yahoo.com.br>
2022-06-30 17:15:10 -0300

    Esta é uma atualização do exercício sintaxe e operadores

--- Com HTML e CSS/Assets/Js/index.js
+++ Com HTML e CSS/Assets/Js/index.js
@@ -1,37 +1,25 @@
-let num1 = "";
-let num2 = "";
-let numero1 = 0;
-let numero2 = 0;
+let n1 = document.getElementById("numero1");
+let n2 = document.getElementById("numero2");
+let resultado = document.getElementById("result");
+let num1 =0; 
+let num2 = 0;
 
 function capturar(){
-    num1 = document.getElementById("numero1").value;
-    num2 = document.getElementById("numero2").value;
+      
+    num1 = n1.value;
+    num2 = n2.value;
     
-    numero1 = Number(num1);
-    numero2 = Number(num2);
-    comparacaoNum(numero1,numero2);
+    const primeirafrase = criaPrimeiraFrase(num1, num2);
+    const segundafrase = criaSegundaFrase(num1, num2);
 
+    return resultado.innerHTML = `${primeirafrase} ${segundafrase}`;
 }
-function comparacaoNum(num1,num2){
-    if(num1!==0){
-        if(num2!==0 ){
-          if(!num1||!num2)  return document.getElementById("result").innerHTML = "Defina dois números!";
-        }
-    }else {
-        imprimir();
-    }
-}
-function imprimir(){
-    const primeirafrase = criaPrimeiraFrase(numero1, numero2);
-    const segundafrase = criaSegundaFrase(numero1, numero2);
 
-    return document.getElementById("result").innerHTML =  `${primeirafrase} ${segundafrase}`;
 
-}
 function criaPrimeiraFrase(num1,num2){
     let nSaoiguais = '';
 
-    if(num1!==num2){
+    if(num1 !== num2){
         nSaoiguais = 'não';
     }
 
@@ -39,7 +27,7 @@ function criaPrimeiraFrase(num1,num2){
 }
 
 function criaSegundaFrase(num1,num2){
-    const soma = num1+num2;
+    const soma = Number(num1)+Number(num2);
     let resultado10 = 'menor';
     let resultado20 = 'menor';
     const compara10 = soma>10;
@@ -54,8 +42,113 @@ function criaSegundaFrase(num1,num2){
     }
 
     return `Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20.`
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = "";
+// let num2 = "";
+// let numero1 = 0;
+// let numero2 = 0;
+
+// function capturar(){
+//     num1 = document.getElementById("numero1").value;
+//     num2 = document.getElementById("numero2").value;
+    
+//     numero1 = Number(num1);
+//     numero2 = Number(num2);
+//     comparacaoNum(numero1,numero2);
+
+// }
+// function comparacaoNum(num1,num2){
+//     if(num1!==0){
+//         if(num2!==0 ){
+//           if(!num1||!num2)  return document.getElementById("result").innerHTML = "Defina dois números!";
+//         }
+//     }else {
+//         imprimir();
+//     }
+// }
+// function imprimir(){
+//     const primeirafrase = criaPrimeiraFrase(numero1, numero2);
+//     const segundafrase = criaSegundaFrase(numero1, numero2);
+
+//     return document.getElementById("result").innerHTML =  `${primeirafrase} ${segundafrase}`;
+
+// }
+// function criaPrimeiraFrase(num1,num2){
+//     let nSaoiguais = '';
+
+//     if(num1!==num2){
+//         nSaoiguais = 'não';
+//     }
+
+//     return`Os números ${num1} e ${num2} ${nSaoiguais} são iguais.`
+// }
+
+// function criaSegundaFrase(num1,num2){
+//     const soma = num1+num2;
+//     let resultado10 = 'menor';
+//     let resultado20 = 'menor';
+//     const compara10 = soma>10;
+//     const compara20 = soma>20;
+
+//     if(compara10){
+//         resultado10 = 'maior';
+//     }
+
+//     if(compara20){
+//         resultado20 = 'maior';
+//     }
+
+//     return `Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20.`
+
+// }
 
     
 
